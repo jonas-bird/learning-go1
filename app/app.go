52fc14ca68f636c3c7900ddef8d7f1e5cea579a1
Jonas Bird <jonas.bird.m@gmail.com>
2022-08-16 22:40:39 -0400

    FEAT add experiment with an infinite loop

--- app/app.go
+++ app/app.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	customers := getData()
@@ -8,14 +11,28 @@ func main() {
 }
 
 // A basic function that will retrieve data
-func getData()(customers []string) {
+func getData() (customers []string) {
 	// Create an array of customer names
 	customers = []string{"John Doe", "Jane Doe", "Robert Smith"}
 
 	// Add elements to the slice
 	customers = append(customers, "Jonas Bird")
+	customers = append(customers, "Peter Murphy")
+	customers = append(customers, "Robert Smith")
+	customers = append(customers, "Daniel Ash")
+	customers = append(customers, "Kirk Brandon")
+	customers = append(customers, "Nik Wade")
+	customers = append(customers, "Jaz Coleman")
+	customers = append(customers, "Carl McCoy")
+	customers = append(customers, "Rozz William")
+	customers = append(customers, "Gavin Friday")
+
+	// Experiment with an infinite loop
+	for {
+		fmt.Println("Infinite loop 1")
+		time.Sleep(time.Second)
+	}
 
 	// return the array to the calling function
 	return customers
-	}
-
+}
